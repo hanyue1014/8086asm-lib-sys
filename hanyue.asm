21b4edf417e7a25e3ffe7d8c4f7ea5d34e465533
hanyue1014 <wongxuanbin@gmail.com>
2022-09-11 18:01:43 +0800

    Add create member file function

--- hanyue.asm
+++ hanyue.asm
@@ -12,12 +12,28 @@
   passBuf         db      9 dup(?)
   password        db      "abcd1234", 0
 
-  ; ---------------- COMMON VAR FOR FILE HANDLING ------------------
+  ; ---------------- var for create member file --------------------
+  ; ---------------- AND                        --------------------
+  ; ---------------- var for open member file   --------------------
+  ; data structure to store user inputted file name (user id inputted by user)
+  MEM_FILE        label   byte
+  memFileMaxLen   db      7                ; max is 6, 7th for enter char
+  memFileActLen   db      ?
+  memFileName     db      "XXXXXX.txt", 0  ; 6 X to be replaced with uID
+  memNotEnfChar   db      "User ID does not contain 6 characters!!!$"
+
+  ; ---------------- VAR FOR file operation function ---------------
   fileHandle      dw      ?
   fileMsg         db      ?
+  readFileErrMsg  db      "Read file failed, please try again$"
+  closeFileErrMsg db      "Close file failed, ignoring$"
+
+  ; --------------- CREATEMEMBER VARS ------------------------------
+  createMemPrompt db      "Enter the user id desired: $"
 
   ; --------------- VAR FOR TESTING TODO: REMOVE -------------------
   testFile        db      "test.txt", 0
+  testCreate      db      "testCr.txt", 0
 
   ; ---------------- VARS FOR MEMBER OPTIONS -----------------------
 
@@ -85,20 +101,46 @@ setCursp macro page, row, col
 
 endm setCursp
 
-; opens the file and set the global variable fileHandle if open successfully
-; if open fail, carry flag will be on
+; creates the file and set the variable passed to handle if create successfully
+; if file already exists, the old file will be deleted without any warning, so please do check
+; upon using this macro, please check CF
+; if create fail, carry flag will be on and variable passed to handle will be the error code
+; Parameter: fileName -> 0 terminated string containing the file name of the file to be created
+;            attrib   -> attribute for the file, 0 for none, 1 for readonly, 2 for hidden, 4 for system, 7 for all, 16 for archive
+;            handle   -> variable to store the file handle in
+createFile macro fileName, attrib, handle
+  push  cx
+  push  dx
+
+  ; attempt to create file
+  mov   cx, attrib      ; file attributes
+  lea   dx, fileName
+  mov   ah, 3ch         ; interrupt to create file
+  int   21h
+  mov   handle, ax
+
+  pop   dx
+  pop   cx
+
+endm createFile
+
+; opens the file and set the variable passed to handle if open successfully
+; upon using this macro, please check CF
+; if open fail, carry flag will be on and variable passed to handle will be the error code
 ; Parameter: fileName -> 0 terminated string containing the file name of the file to be opened
-openFile macro fileName
+;            mode     -> mode to open the file in, 0 for r, 1 for w, 2 for rw
+;            handle   -> variable to store the file handle in
+openFile macro fileName, mode, handle
 
   push  ax
   push  dx
 
   ; open file (TODO: Check CF for success: CF on if fail, err code at AX, if success, AX is file handle)
-  mov   ah, 3dh       ; open file instruction
-  mov   al, 2         ; open in read, write mode
+  mov   ah, 3dh           ; open file instruction
+  mov   al, mode          ; open in read, write mode
   lea   dx, fileName
   int   21h
-  mov   fileHandle, ax
+  mov   handle, ax
 
   pop   dx
   pop   ax
@@ -228,26 +270,79 @@ moveFileCursEnd proc
   ret
 moveFileCursEnd endp
 
-; this function assumes the handler for the file is stored in the fileHandle global variable
+; THIS FUNCTION DOES NOT RETAIN REGISTER VALUES, REGISTER WILL BE OVERWRITTEN UPON USING THIS
+; this function assumes the handler for the file to read is already in bx
 ; prints the content in the file until EOF
 printFile proc
 
   ; read file (CF on if fail, err code at AX, if success, AX is num bytes actually read, 0 IF when call eh time alrd at EOF, rmb move the cursor back to position 0 first when repeated read without reopening)
   READ_FILE_CHAR:
-    mov   bx, fileHandle
+    ; address of the place to store the content read
     lea   dx, fileMsg
-    mov   cx, 1         ; num bytes to read (will be reading byte by byte repeatedly until EOF)
-    mov   ah, 3fh       ; read file instruction
+    mov   cx, 1           ; num bytes to read (will be reading byte by byte repeatedly until EOF)
+    mov   ah, 3fh         ; read file instruction
     int   21h
+    jc    READ_FILE_FAIL  ; cf on, read file failed 
     ; compare if at EOF, if no, print the character read and loop again
     cmp   ax, 0
     je    PRINT_FILE_END
     printChar fileMsg
     jmp   READ_FILE_CHAR
+  
+  READ_FILE_FAIL:
+    printStr readFileErrMsg
+    jmp   PRINT_FILE_END
 
   PRINT_FILE_END:  
     ret
 printFile endp
+
+; always close a file if u opened it
+; this function overwrites register values
+; this function assumes bx is set to the handle of the file to be closed
+closeFile proc
+
+  mov     ah, 3eh
+  int     21h
+  jc      CLOSE_FILE_FAILED
+  jmp     CLOSE_FILE_END
+
+  CLOSE_FILE_FAILED:
+    printStr closeFileErrMsg
+
+  CLOSE_FILE_END:
+    ret
+closeFile endp
+
+createMem proc
+
+  CREATE_MEM_START:
+    printStr createMemPrompt
+    ; input user id (will be used as the file name)
+    mov     ah, 0ah   ; input str function
+    lea     dx, MEM_FILE
+    int     21h
+    call    newline
+    ; if not enough six character, prompt the user
+    cmp     memFileActLen, 6
+    jl      USER_ID_NT_ENF_CH
+    ; it wont be longer than 7 as max len is 7
+    jmp     USER_ID_ENF_CH
+
+  USER_ID_NT_ENF_CH:
+    printStr memNotEnfChar
+    jmp      CREATE_MEM_START
+
+  USER_ID_ENF_CH:
+    ; set the . back (previously replaced by enter key)
+    mov      memFileName[6], "."
+    ; TODO: Use open file to check if user already exist, for now assume user not exist if open fail
+    createFile memFileName, 0, fileHandle
+  
+  CREATE_MEM_END:
+    ret
+
+createMem endp
 
 ;------- main function ------------------------------------
 main proc far
@@ -258,8 +353,16 @@ main proc far
   
   ; the real program is actually here
   
-  openFile testFile
-  call     printFile
+  ; openFile testFile, 2, fileHandle
+  ; createFile testCreate, 0, fileHandle
+  
+
+  ; printFile assumes the file handle for the file to be printed is already in bx when called
+  ; mov      bx, fileHandle
+  ; call     printFile
+
+  mov      bx, fileHandle
+  call     closeFile
     
   ; end of real program
   
